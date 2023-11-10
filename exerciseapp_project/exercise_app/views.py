fc288167bfba2b9ac7b30cd19fcf379b52cd4255
bethjm <b.jeanne.mills@gmail.com>
2023-11-10 08:30:56 -0500

    updating ciews

--- exerciseapp_project/exercise_app/views.py
+++ exerciseapp_project/exercise_app/views.py
@@ -2,14 +2,14 @@ from rest_framework import generics
 from .models import Cue, Exercise, Stretch
 from .serializers import CueSerializer, ExerciseSerializer, StretchSerializer
 
-class ListCues(generics.ListAPIView):
-    queryset = Cue.objects.all()
-    serializer_class = CueSerializer
+# class ListCues(generics.ListAPIView):
+#     queryset = Cue.objects.all()
+#     serializer_class = CueSerializer
 
 class ListExercises(generics.ListAPIView):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
-class ListStretches(generics.ListAPIView):
-    queryset = Stretch.objects.all()
-    serializer_class = StretchSerializer
+# class ListStretches(generics.ListAPIView):
+#     queryset = Stretch.objects.all()
+#     serializer_class = StretchSerializer
